a51fb5326795fea2e73ad1bc854577eac9f65fe2
Henri ahl <hgstah@utu.fi>
2014-04-28 17:10:55 +0300

    Added function for comparing hands

--- PokerHand.hs
+++ PokerHand.hs
@@ -1,6 +1,7 @@
 module PokerHand (
   Hand(..),
   HandCateg(..),
+  compareHands,
   evaluateHand,
   highCardValue
 ) where
@@ -21,6 +22,75 @@ data HandCateg = HighCard
                | StraightFlush
                | RoyalFlush
                deriving (Eq, Ord, Bounded, Enum, Show, Read)
+
+compareHands :: Hand -> Hand -> Ordering
+compareHands [] [] = EQ
+compareHands xs [] = GT
+compareHands [] ys = LT
+compareHands xs ys
+    | evaluateHand xs /= evaluateHand ys = (evaluateHand xs) `compare` (evaluateHand ys)
+    | otherwise = case evaluateHand xs
+                  of   HighCard      -> compareHighCards xs ys
+                       Pair          -> compareNOfKind 2 xs ys
+                       TwoPair       -> compareNOfKind 2 xs ys
+                       ThreeOfAKind  -> compareNOfKind 3 xs ys
+                       Straight      -> compareHighCards xs ys
+                       Flush         -> compareHighCards xs ys
+                       FullHouse     -> compareFullHouse xs ys
+                       FourOfAKind   -> compareNOfKind 4 xs ys
+                       StraightFlush -> compareHighCards xs ys
+                       RoyalFlush    -> EQ
+
+compareHighCards :: Hand -> Hand -> Ordering
+compareHighCards [] [] = EQ
+compareHighCards xs [] = GT
+compareHighCards [] ys = LT
+compareHighCards xs ys
+    | last valx /= last valy = (last valx) `compare` (last valy)
+    | otherwise = compareHighCards (init xs) (init ys)
+    where
+        valx = sort (values xs)
+        valy = sort (values ys)
+
+{-
+ - Compares the n-of-a-kind hands of two players. The one who has
+ - n cards of the same higher face value is judged greater. If
+ - both players have n cards of the same value, then the remaining
+ - high cards determine the ordering. This works also for
+ - comparing hands of two pairs with n=2 because a list is made
+ - of the values of the pairs and then compared using
+ - compareSortedValues.
+ -
+ - See: compareSortedValues
+-}
+compareNOfKind :: Int -> Hand -> Hand -> Ordering
+compareNOfKind _ [] [] = EQ
+compareNOfKind _ xs [] = GT
+compareNOfKind _ [] ys = LT
+compareNOfKind n xs ys
+    | valOfKind valx /= valOfKind valy = (valOfKind valx) `compareSortedValues` (valOfKind valy)
+    | otherwise = (valHigh valx) `compareSortedValues` (valHigh valy)
+    where
+        valx = sort (values xs)
+        valy = sort (values ys)
+        valOfKind xs = [head x | x <- (group xs), length x == n]
+        valHigh xs = [head x | x <- (group xs), length x == 1]
+
+compareFullHouse :: Hand -> Hand -> Ordering
+compareFullHouse [] [] = EQ
+compareFullHouse xs [] = GT
+compareFullHouse [] ys = LT
+compareFullHouse xs ys
+    | compareNOfKind 3 xs ys /= EQ = compareNOfKind 2 xs ys
+    | otherwise = compareNOfKind 3 xs ys
+
+compareSortedValues :: [Value] -> [Value] -> Ordering
+compareSortedValues [] [] = EQ
+compareSortedValues xs [] = GT
+compareSortedValues [] ys = LT
+compareSortedValues xs ys
+    | head xs == head ys = compareSortedValues (tail xs) (tail ys)
+    | otherwise = (head xs) `compare` (head ys)
 
 evaluateHand :: Hand -> HandCateg
 evaluateHand [] = error "Empty hand"
