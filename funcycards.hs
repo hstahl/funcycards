e9d9d2d093034fde43dbd0dbb2e26e26b17b692f
Henri ahl <hgstah@utu.fi>
2014-05-03 16:15:39 +0300

    skeleton of command line interface to game

---
+++ funcycards.hs
@@ -0,0 +1,28 @@
+{-
+ - A command line interface for a poker game.
+ -
+ - Input: Names of players (1-5)
+ -        Empty line when done
+ -
+ - Each player on their turn will select
+ - the cards they want to discard
+ -
+ - After discarding, the hands are filled
+ - up to 5 cards each again and the winner
+ - is announced.
+ -}
+
+import Card
+import Deck
+import PokerHand
+import Player
+import Game
+
+main = do
+    --create game
+    --ask player names and add to game
+    --show hand and ask for discards
+    --fill hands up to 5 cards
+    --show final hands
+    --announce winner
+    return ()
